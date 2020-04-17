8160913f55e2b1fd8938bcd7935679c1018f3bae
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2020-04-17 03:24:32 -0300

    Linux support

--- library_and_visibility/library/src/Core.h
+++ library_and_visibility/library/src/Core.h
@@ -5,11 +5,24 @@
 #ifndef LIBRARY_AND_VISIBILITY_CORE_H
 #define LIBRARY_AND_VISIBILITY_CORE_H
 
-// #define DLLEXPORT
-#if COMPILING_DLL
-#define DLLEXPORT __declspec(dllexport)
+#if defined(_MSC_VER)
+    // #define DLLEXPORT
+    #if COMPILING_DLL
+    #define DLLEXPORT __declspec(dllexport)
+    #else
+    #define DLLEXPORT __declspec(dllimport)
+    #endif
+#elif defined(__GNUC__)
+    //  GCC
+    #if COMPILING_DLL
+    #define DLLEXPORT  __attribute__((visibility("default")))
+    #else
+    #define DLLEXPORT
+    #endif
 #else
-#define DLLEXPORT __declspec(dllimport)
+    //  do nothing and hope for the best?
+    #define DLLEXPORT
+    #pragma warning Unknown dynamic link import/export semantics.
 #endif
 
 #endif //LIBRARY_AND_VISIBILITY_CORE_H
